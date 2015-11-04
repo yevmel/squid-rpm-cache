3451f52a226f225ec2fa4153a71f8144092f8bf5
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2015-11-04 22:46:38 +0100

    added missing new-line

--- store_id_program/main.go
+++ store_id_program/main.go
@@ -19,9 +19,9 @@ func main() {
         filename := re.FindString(url)
 
         if len(filename) > 0 {
-            fmt.Printf("OK store-id=%v", filename);
+            fmt.Printf("OK store-id=%v\n", filename);
         } else {
-            fmt.Printf("OK store-id=%v", url);
+            fmt.Printf("OK store-id=%v\n", url);
         }
     }
 }
